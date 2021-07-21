85748645ca0a5900eb5bf809ed565d2398104a72
H. Utku Maden <utkumaden@hotmail.com>
2021-07-21 20:08:03 +0300

    Write some of the driver functions.

--- include/wsen-pads.h
+++ include/wsen-pads.h
@@ -55,4 +55,112 @@ int wsen_pads_configureInterface(
     int int_od
 );
 
+/**
+ * Set or reset interrupts.
+ * @param driver Driver instance.
+ * @param mux Interrupt mux selection.
+ * @param fifo_full Interrupt when FIFO full.
+ * @param fifo_threshold Interrupt when FIFO threshold level reached.
+ * @param fifo_overflow Interrupt when FIFO overflows.
+ * @param data_ready Interrupt when pressure data ready.
+ * @param latch Latch interrupt pin until read.
+ * @return -1 on error.
+ * @remarks
+ * The interrupt mux determines which interrupts are propogated. When
+ * differential pressure interrupts are selected, no other interrupt is sent.
+ */
+int wsen_pads_setInterrupt(
+    wsen_pads_t driver,
+    wsen_pads_interrupt_mux_t mux,
+    int fifo_full,
+    int fifo_threshold,
+    int fifo_overflow,
+    int data_ready,
+    int latch
+);
+
+/**
+ * Get device status bits.
+ * @param driver Driver instance.
+ * @param out_status Status bits.
+ * @param opt_out_fifo_level (Optional) FIFO fill level.
+ * @return -1 on error.
+ *
+ * @remarks Clears interrupt status. FIFO fill level is also read as an
+ * optimization, which can be written to pointer if wanted.
+ */
+int wsen_pads_getStatus(wsen_pads_t driver, wsen_pads_status_t *out_status, uint8_t *opt_out_fifo_level);
+
+/**
+ * Configure data mode of device.
+ * @param driver Driver instance.
+ * @param dataRate Output data rate.
+ * @param lowpass Low pass mode.
+ * @param low_noise Enable low noise mode.
+ * @param blockDataUpdate Data registers keep their values until read when set.
+ * @return -1 on error.
+ * @remarks Sensor enters powerdown mode before configuring data modes.
+ */
+int wsen_pads_configureData(
+    wsen_pads_t driver,
+    wsen_pads_odr_t dataRate,
+    wsen_pads_lowpass_t lowpass,
+    int low_noise,
+    int blockDataUpdate
+);
+
+/**
+ * Enable autofeatures of the sensor.
+ * @param driver Driver instance.
+ * @param autozero Enable or reset AUTOZERO mode.
+ * @param autorefp Enable or reset AUTOREFP.
+ * @return -1 on error.
+ * @remarks Refer to sensor documentation for feature descriptions. AUTOZERO and
+ * AUTOREFP cannot be set at the same time.
+ */
+int wsen_pads_autofeature(wsen_pads_t driver, int autozero, int autorefp);
+
+/**
+ * Convert numeric to reference pressure value.
+ * @param value Value to convert.
+ * @return Converted value.
+ */
+#define WSEN_PADS_TO_REFERENCE_PRESSURE(value) ((uint16_t)((value) / 16))
+
+/**
+ * Set pressure threshold used with differential pressure interrupts.
+ * @param driver Driver instance.
+ * @param pressure Pressure threshold value in kPa / 16.
+ * @return -1 on error.
+ * @remarks Pressure threshold value is written to sensor as fixed point value.
+ * The value is in 62.5 Pa increments.
+ */
+int wsen_pads_setPressureThresholdR(wsen_pads_t driver, uint16_t pressure);
+
+/**
+ * Set pressure threshold used with differential pressure interrupts.
+ * @param driver Driver instance.
+ * @param pressure Pressure threshold value in Pa.
+ * @return -1 on error.
+ * @remarks Pressure threshold value is written to sensor as fixed point value.
+ * The value is in 62.5 Pa increments.
+ */
+inline static int wsen_pads_setPressureThresholdI(wsen_pads_t driver, uint32_t pressure)
+{
+    return wsen_pads_setPressureThresholdR(driver, WSEN_PADS_TO_REFERENCE_PRESSURE(pressure / 1000));
+}
+
+/**
+ * Set pressure threshold used with differential pressure interrupts.
+ * @param driver Driver instance.
+ * @param pressure Pressure threshold value in Pa.
+ * @return -1 on error.
+ * @remarks Pressure threshold value is written to sensor as fixed point value.
+ * The value is in 62.5 Pa increments.
+ */
+inline static int wsen_pads_setPressureThresholdF(wsen_pads_t driver, float pressure)
+{
+    return wsen_pads_setPressureThresholdR(driver, WSEN_PADS_TO_REFERENCE_PRESSURE(pressure / 1000));
+}
+
 #endif
--- include/wsen-pads/bits.h
+++ include/wsen-pads/bits.h
@@ -12,6 +12,48 @@ typedef enum wsen_pads_device_addr_t
     WSEN_PADS_I2C_ADDR_HI       = 0xBA
 } wsen_pads_device_addr_t;
 
+typedef enum wsen_pads_odr_t 
+{
+    WSEN_PADS_DATARATE_OFF_OR_SINGLE    = 0,
+    WSEN_PADS_DATARATE_1HZ              = 1,
+    WSEN_PADS_DATARATE_10HZ             = 2,
+    WSEN_PADS_DATARATE_25HZ             = 3,
+    WSEN_PADS_DATARATE_50HZ             = 4,
+    WSEN_PADS_DATARATE_75HZ             = 5,
+    WSEN_PADS_DATARATE_100HZ            = 6,
+    WSEN_PADS_DATARATE_200HZ            = 7,
+    WSEN_PADS_DATARATE_MAX              = WSEN_PADS_DATARATE_200HZ
+} wsen_pads_odr_t;
+
+typedef enum wsen_pads_lowpass_t
+{
+    WSEN_PADS_LOWPASS_OFF       = 0,
+    WSEN_PADS_LOWPASS_SINGLE    = 1,
+    WSEN_PADS_LOWPASS_DUAL      = 2
+} wsen_pads_lowpass_t;
+
+typedef enum wsen_pdas_interrupt_mux_t
+{
+    WSEN_PADS_INTERRUPT_MUX_OTHER                   = 0,
+    WSEN_PADS_INTERRUPT_MUX_PRESSURE_HIGH           = 1,
+    WSEN_PADS_INTERRUPT_MUX_PRESSURE_LOW            = 2,
+    WSEN_PADS_INTERRUPT_MUX_PRESSURE_HIGH_OR_LOW    = 3
+} wsen_pads_interrupt_mux_t;
+
 #define WSEN_PADS_FIFO_CAPACITY 128 /**< Maximum number of samples the FIFO can hold. */
+
+typedef enum wsen_pads_status_t
+{
+    WSEN_PADS_STATUS_PRESSURE_READY         = (1 << 0),
+    WSEN_PADS_STATUS_TEMPERATURE_READY      = (1 << 1),
+    WSEN_PADS_STATUS_PRESSURE_OVERRUN       = (1 << 2),
+    WSEN_PADS_STATUS_TEMPERATURE_OVERRUN    = (1 << 3),
+    WSEN_PADS_STATUS_FIFO_THRESHOLD         = (1 << 4),
+    WSEN_PADS_STATUS_FIFO_OVERRUN           = (1 << 5),
+    WSEN_PADS_STATUS_FIFO_FULL              = (1 << 6),
+    WSEN_PADS_STATUS_INT_ACTIVE             = (1 << 7),
+    WSEN_PADS_STATUS_INT_PRESSURE_LOW       = (1 << 8),
+    WSEN_PADS_STATUS_INT_PRESSURE_HIGH      = (1 << 9)
+} wsen_pads_status_t;
 
 #endif
